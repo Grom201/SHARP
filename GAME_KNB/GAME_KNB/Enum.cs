db14aaf7ef7260769e3ef39778d0a2d4db1b7de4
Grom201 <40233575+Grom201@users.noreply.github.com>
2020-03-06 15:30:50 +0200

    Add files via upload

---
+++ GAME_KNB/GAME_KNB/Enum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAME_KNB
+{
+    public class Enum
+    {
+        public enum Variant
+
+            {
+            Rock=1,
+            Scissors=2,
+            Paper=3
+        }
+    }
+}
---
+++ GAME_KNB/GAME_KNB/Game.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GAME_KNB;
+
+
+namespace GAME_KNB
+{
+
+
+
+    class Game
+    {
+        int countWins = 0;
+        int countLoses = 0;
+        int countDraws = 0;
+
+        static int youChoose;
+
+      public  static int computerChoose;
+      
+
+
+    
+        
+        public static int Choose(int i)
+        {
+            switch (i)
+            {
+                case 3:
+                    computerChoose = Random();
+                    youChoose = (int)Enum.Variant.Paper;
+                    break;
+                case 1:
+                    computerChoose = Random();
+                    youChoose = (int)Enum.Variant.Rock;
+                    break;
+                case 2:
+                    computerChoose = Random();
+                    youChoose = (int)Enum.Variant.Scissors;
+                    break;
+            }
+            return youChoose;
+        }
+
+        public void Play()
+        {
+        
+            if (youChoose == 1 & computerChoose == 2)
+                countWins++;
+            else if (youChoose == 1 & computerChoose == 3)
+                countLoses++;
+            else if (youChoose == 1 & computerChoose == 1)
+            {
+                countDraws++;
+            }
+
+            if (youChoose == 2 & computerChoose == 3)
+                countWins++;
+            else if (youChoose == 2 & computerChoose == 1)
+                countLoses++;
+            else if (youChoose == 2 & computerChoose == 2)
+            {
+                countDraws++;
+            }
+            if (youChoose == 3 & computerChoose == 1)
+            {
+                countWins ++;
+            }
+            else if (youChoose == 3 & computerChoose == 2)
+            { 
+            countLoses++;
+        }
+            else if (youChoose == 3 & computerChoose == 3)
+            {
+                countDraws++;
+            }
+
+           // Console.WriteLine($"Wins= {countWins}\nLoses = {countLoses}\nDraws= {countDraws}");
+       }
+        public void CountRounds()
+        {
+            Console.WriteLine($"Wins= {countWins}\nLoses = {countLoses}\nDraws= {countDraws}");
+        }
+
+
+
+        public static int Random()
+        {
+            Random random = new Random();
+            int r = random.Next(1,4);
+            return r;
+        }
+    }
+}
